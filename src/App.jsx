88c3651308f9b644c0c20ff5d38d8721ec5bed69
Jordan <jordanbaje@hotmail.com>
2025-05-14 17:40:28 -0300

    sessoes iniciado

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <Container>
     <Topo/>
-    <Menu/>
+    {/* <Menu/> */}
     <Sessoes/>
     </Container>
   )
--- src/components/sessoes.jsx
+++ src/components/sessoes.jsx
@@ -1,9 +1,99 @@
 import styled from "styled-components";
 import axios from "axios";
+import { useState, useEffect } from 'react';
 
 export default function Sessoes (){
 
-return
+return (
+    <SessoesContainer>
+    <h1>Selecione o horário</h1>
+    
+    <Sessao>
 
+        <Data>
+            
+            <p>Quinta-feira, 21/03/2024</p>
+
+        </Data>
+        <Linha />
+        <Horarios>
+            <p>10:00</p>
+            
+        </Horarios>
+
+    </Sessao>
+    
+   
+  
+    </SessoesContainer>
+
+)
 
 }
+
+
+
+const SessoesContainer = styled.div`
+  background-color: rgba(33, 34, 38, 1);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+ /* overflow-y: scroll; */
+  height: 100%;
+  max-height: 800px;
+  padding: 30px;
+
+
+h1 {font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    font-size: 24px;
+    font-weight: 400;
+    color: white;
+}
+
+`
+const Sessao = styled.div`
+  width: 340px;
+  height: 150px;
+  background-color: rgba(43, 45, 54, 1);
+  display: flex;
+  flex-direction: column;
+  align-items: first baseline;
+  justify-content: center;
+  margin-top: 20px;
+  padding-left: 20px;
+  
+`;
+
+const Data = styled.div`
+  margin-bottom: 10px;
+  
+
+  p {
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    font-size: 20px;
+    font-weight: 400;
+    color: white;
+  }
+`;
+
+const Horarios = styled.div`
+  display: flex;
+  gap: 10px;
+
+  p {
+    border: 2px solid rgba(238, 137, 127, 1);
+    color: rgba(238, 137, 127, 1);
+    padding: 10px 20px;
+    border-radius: 4px;
+    font-weight: bold;
+    font-size: 16px;
+  }
+
+`;
+
+const Linha = styled.hr`
+  width: 80%;
+  border: none;
+  border-top: 1px solid rgba(78, 90, 101, 1);
+  margin: 20px 0;
+`;
